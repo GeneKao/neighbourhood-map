fc0456a74bff8dccc4c97f0829e42d957f930881
Gene Kao <kao.gene@gmail.com>
2018-05-05 19:04:43 +0200

    Add map styles.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 var map;
+
+
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 48.778761, lng: 9.179869},
-        zoom: 17
+        zoom: 17,
+        styles: styles
     });
+
     var schlossplatz = {lat: 48.778761, lng: 9.179869};
+
     var marker = new google.maps.Marker({
         position: schlossplatz,
         map: map,
         title: 'First Marker!'
     });
+
+
+    ko.applyBindings(new ViewModel());
 }
+
+
+var ViewModel = function() {
+    var self = this;
+
+
+
+};
---
+++ js/map-styles.js
@@ -0,0 +1,66 @@
+var styles = [
+    {
+        featureType: 'water',
+        stylers: [
+            { color: '#19a0d8' }
+        ]
+    },{
+        featureType: 'administrative',
+        elementType: 'labels.text.stroke',
+        stylers: [
+            { color: '#ffffff' },
+            { weight: 6 }
+        ]
+    },{
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+            { color: '#e85113' }
+        ]
+    },{
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+            { color: '#efe9e4' },
+            { lightness: -40 }
+        ]
+    },{
+        featureType: 'transit.station',
+        stylers: [
+            { weight: 9 },
+            { hue: '#e85113' }
+        ]
+    },{
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+            { visibility: 'off' }
+        ]
+    },{
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [
+            { lightness: 100 }
+        ]
+    },{
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+            { lightness: -100 }
+        ]
+    },{
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+            { visibility: 'on' },
+            { color: '#f0e4d3' }
+        ]
+    },{
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [
+            { color: '#efe9e4' },
+            { lightness: -25 }
+        ]
+    }
+];
